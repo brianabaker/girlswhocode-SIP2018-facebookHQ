68886910a37adeeeccc096c4a92efe748361286a
brianabaker <brianaclairbaker@gmail.com>
2018-06-27 06:22:35 -0700

    making sure all changes commited

--- python/python-solutions/chatbot/chatbot-pt1.5-errormessage.py
+++ python/python-solutions/chatbot/chatbot-pt1.5-errormessage.py
@@ -1,36 +1,29 @@
-# --- Define your functions below! ---
-
-#  BROKEN
-
 # The chatbot introduces itself and gives the user instructions.
 def intro():
   print("Hi, my name is Phyllis. Let's talk!")
   print("Type something and hit enter.")
 
-# Choose a response based on the user's input.
-def process_input(answer):
+ # gives NameError: name 'answer' is not defined
+ # let's talk about scope!
+
+def process_input():
   if answer == "hi":
     say_greeting()
   else:
     say_default()
 
-# Display a greeting message to the user.
 def say_greeting():
   print("Hey there!")
 
-# Display a default message to the user.
 def say_default():
   print("That's cool!")
 
-
-# --- Put your main program below! ---
 def main():
   intro()
   while True:
     answer = input("(What will you say?) ")
-    process_input(answer)
-
+    process_input()
 
 # DON'T TOUCH! Setup code that runs your main() function.
 if __name__ == "__main__":
-  main()
+    main()
