b5f715c4ddec2ac48b76983e6d06e7c87ec0cb36
brianabaker <brianaclairbaker@gmail.com>
2018-07-01 18:17:07 -0700

    changed robotics before day one

---
+++ robotics/let-there-be-light/starter-light-pt3.ino
@@ -0,0 +1,13 @@
+
+// for this exercise a unit of time equal to
+int unit = 300;
+
+// write the logic for what functions should run when in the loop, i.e. tell what functions to run when to morse code out the letters
+
+// write a function for each letter --- letters are made of dots and dashes right?
+
+// write a function for dot
+
+// write a function for dash
+
+// call dash and dot in the functions for each letter
